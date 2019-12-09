90944ecac33ac590f66495926f3e2c7553bd6d0b
Nicklas <nnicklas92@gmail.com>
2019-12-09 08:33:16 +0100

    Graceful shutdown

--- Dream-Stream-Producer/Program.cs
+++ Dream-Stream-Producer/Program.cs
@@ -8,6 +8,9 @@ namespace Producer
 {
     internal class Program
     {
+        private static bool _run = true;
+        private static ProducerService _producer;
+
         private static async Task Main()
         {
             Variables.AmountOfMessagesVariable = 100000;
@@ -16,18 +19,19 @@ namespace Producer
             Variables.IsDev = true;
             const string topic = "Topic3";
 
-            var producer = await ProducerService.Setup("http://localhost");
+            _producer = await ProducerService.Setup("http://localhost");
             var messages = MessageGenerator.GenerateMessages(Variables.AmountOfMessagesVariable);
-            var messageHeaders = await producer.GetMessageHeaders(messages, topic);
-
-            while (true)
+            var messageHeaders = await _producer.GetMessageHeaders(messages, topic);
+            AppDomain.CurrentDomain.ProcessExit += ShutdownFunction;
+            
+            while (_run)
             {
                 var sw = new Stopwatch();
                 sw.Reset();
                 sw.Start();
 
                 var tasks = Enumerable.Range(0, messageHeaders.Length) 
-                    .Select(i => producer.Publish(messageHeaders[i], messages[i]));
+                    .Select(i => _producer.Publish(messageHeaders[i], messages[i]));
                 await Task.WhenAll(tasks);
 
                 sw.Stop();
@@ -35,6 +39,13 @@ namespace Producer
 
                 await Task.Delay(500); //Delay added for test of timer on batches
             }
+        }
+
+        private static async void ShutdownFunction(object sender, EventArgs e)
+        {
+            _run = false;
+            await _producer.CloseConnections();
+            Console.WriteLine("Process is exiting!");
         }
     }
 }
